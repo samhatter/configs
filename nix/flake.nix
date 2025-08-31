a640b2339e823013e19b4c7f98d6926976c779d8
Samantha Vincent <sammyvincent2001@gmail.com>
2025-08-30 19:44:20 -0700

    adding plex-desktop forked nixpkgs

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,24 +1,32 @@
 {
-    description = "NixOS Config";
+  description = "NixOS Config";
 
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        sops-nix.url = "github:Mic92/sops-nix";
-        sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-fork.url = "github:samhatter/nixpkgs?ref=adding-StartupWMClass-to-plex-desktop";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, ... }:
-    {
-        nixosConfigurations = {
-            snapcaster = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-
-                modules = [
-                    sops-nix.nixosModules.sops
-                    ./hosts/snapcaster/configuration.nix
-                ];
-            };
+  outputs = {
+    nixpkgs,
+    nixpkgs-fork,
+    sops-nix,
+    ...
+  }: {
+    nixosConfigurations = {
+      snapcaster = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit nixpkgs-fork;
         };
+        modules = [
+          sops-nix.nixosModules.sops
+          ./hosts/snapcaster/configuration.nix
+        ];
+      };
     };
+  };
 }
+
--- nix/profiles/workstation.nix
+++ nix/profiles/workstation.nix
@@ -1,57 +1,70 @@
-{ config, pkgs, lib, ... }:
-
 {
-    services.xserver.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.displayManager.gdm.wayland = true;
-    services.xserver.desktopManager.gnome.enable = true;
-    
+  nixpkgs-fork,
+  pkgs,
+  ...
+}: {
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-    fonts.packages = with pkgs; [
-        ibm-plex
-        dejavu_fonts
-        noto-fonts
-        noto-fonts-emoji
-        fira-code
-    ];
+  fonts.packages = with pkgs; [
+    ibm-plex
+    dejavu_fonts
+    noto-fonts
+    noto-fonts-emoji
+    fira-code
+  ];
 
-    services.libinput.enable = true;
+  services.libinput.enable = true;
 
-    hardware.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-    };
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
-    services.printing.enable = true;
-    services.avahi.enable = true;
+  services.printing.enable = true;
+  services.avahi.enable = true;
 
-    environment.systemPackages = with pkgs; [
-        chromium
-        gnome-tweaks
-        gnomeExtensions.dash-to-dock
-        less
-    ];
-
-    programs.gnome-terminal.enable = true;
-    services.gnome.gnome-keyring.enable = true;
-    services.gnome.gnome-settings-daemon.enable = true;
-    xdg.portal = {
-        extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-        xdgOpenUsePortal = true;
-    };
-    security.pam.services.gdm.enableGnomeKeyring = true;
-    
-    virtualisation.docker = {
-        enable = true;
-        rootless = {
-        enable = true;
-        setSocketVariable = true;
+  nixpkgs.overlays = [
+    (
+      final: prev: let
+        forkPkgs = import nixpkgs-fork {
+          inherit (final) system;
+          config = prev.config;
         };
+      in {plex-desktop = forkPkgs.plex-desktop;}
+    )
+  ];
+
+  environment.systemPackages = with pkgs; [
+    chromium
+    plex-desktop
+    plexamp
+    gnome-tweaks
+    gnomeExtensions.dash-to-dock
+    gnomeExtensions.resource-monitor
+    less
+  ];
+
+  programs.gnome-terminal.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.gnome.gnome-settings-daemon.enable = true;
+  xdg.portal = {
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    xdgOpenUsePortal = true;
+  };
+  security.pam.services.gdm.enableGnomeKeyring = true;
+
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
     };
-
-
+  };
 }
