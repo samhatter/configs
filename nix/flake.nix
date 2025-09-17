81cc4742cc625aab68acbf140abb16cb2764726d
samhatter <sammyvincent2001@gmail.com>
2025-09-17 15:49:41 -0700

    adding sammy-dev ref

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-fork.url = "github:samhatter/nixpkgs?ref=adding-StartupWMClass-to-plex-desktop";
+    nixpkgs-fork.url = "github:samhatter/nixpkgs?ref=sammy-dev";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
