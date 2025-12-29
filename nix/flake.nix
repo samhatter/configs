a6e0024d8d16aeb3e1fafb4aba7fcba2d34dd09f
Samantha Vincent <sammyvincent2001@gmail.com>
2025-12-28 18:14:09 -0800

    adding flake target

--- nix/flake.nix
+++ nix/flake.nix
@@ -53,6 +53,18 @@
         ];
       };
     };
+
+    nixosConfigurations = {
+      arcanis = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit nixpkgs-fork; };
+        modules = [
+          nixos-wsl.nixosModules.default
+          sops-nix.nixosModules.sops
+          ./hosts/arcanis/configuration.nix
+        ];
+      };
+    };
   };
 }
 
