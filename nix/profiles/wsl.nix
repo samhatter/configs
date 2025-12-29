5ab4eeb9673721b484fd3ac585e488e2ce674fa6
samhatter <sammyvincent2001@gmail.com>
2025-12-29 09:51:09 -0800

    enable bootloader settings for WSL profile

--- nix/profiles/wsl.nix
+++ nix/profiles/wsl.nix
@@ -2,5 +2,8 @@
   wsl.enable = true;
   wsl.defaultUser = "samantha";
 
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = false;
+
   programs.nix-ld.enable = true;
 }
