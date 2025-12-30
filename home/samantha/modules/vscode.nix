2cbeb18192b6aad0ac683adaae0ab04c98d741be
Samantha Vincent <sammyvincent2001@gmail.com>
2025-12-30 08:45:12 -0800

    refactor: restructure vscode.nix to improve package import handling

--- home/samantha/modules/vscode.nix
+++ home/samantha/modules/vscode.nix
@@ -1,5 +1,15 @@
-{...}: {
+{
+  pkgs,
+  nixpkgs-fork,
+  ...
+}: let
+  forkPkgs = import nixpkgs-fork {
+    system = pkgs.stdenv.hostPlatform.system;
+    config = pkgs.config;
+  };
+in {
   programs.vscode = {
     enable = true;
+    package = forkPkgs.vscode;
   };
 }
