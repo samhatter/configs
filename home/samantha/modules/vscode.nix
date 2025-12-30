cd217d1df402f674d5af8f4f3205145c2ca5d56e
samhatter <sammyvincent2001@gmail.com>
2025-12-30 09:11:29 -0800

    adding extensions

--- home/samantha/modules/vscode.nix
+++ home/samantha/modules/vscode.nix
@@ -1,15 +1,16 @@
+{pkgs, ...}:
+let
 {
-  pkgs,
-  nixpkgs-fork,
-  ...
-}: let
-  forkPkgs = import nixpkgs-fork {
-    system = pkgs.stdenv.hostPlatform.system;
-    config = pkgs.config;
-  };
-in {
   programs.vscode = {
-    enable = true;
-    package = forkPkgs.vscode;
-  };
+  enable = true;
+  package = pkgs.vscode;
+  extensions = with pkgs.vscode-extensions; [
+    bbenoist.nix
+    ms-python.python
+    ms-azuretools.vscode-docker
+    ms-vscode-remote.remote-ssho
+    golang.go
+    github.copilot
+  ];
+};
 }
