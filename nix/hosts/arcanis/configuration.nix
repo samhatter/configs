22322be87490d7e2de31abaa10600c2d2ca23f67
Samantha Vincent <sammyvincent2001@gmail.com>
2026-01-01 10:48:15 -0800

    enable liveRestore for Docker configuration

--- nix/hosts/arcanis/configuration.nix
+++ nix/hosts/arcanis/configuration.nix
@@ -27,7 +27,7 @@
   virtualisation = {
     docker = {
       enable = true;
-      liveRestore = false;
+      liveRestore = true;
       daemon.settings = {
         experimental = true;
         metrics-addr = "0.0.0.0:9323";
