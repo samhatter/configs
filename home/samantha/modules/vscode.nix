bcf89c777210c0ba3236a29c8a7b73c996ac2779
samhatter <sammyvincent2001@gmail.com>
2026-01-16 07:51:54 -0800

    adding vscode plugins

--- home/samantha/modules/vscode.nix
+++ home/samantha/modules/vscode.nix
@@ -1,16 +1,14 @@
-{pkgs, ...}:
-let
-{
+{pkgs, ...}: {
   programs.vscode = {
-  enable = true;
-  package = pkgs.vscode;
-  extensions = with pkgs.vscode-extensions; [
-    bbenoist.nix
-    ms-python.python
-    ms-azuretools.vscode-docker
-    ms-vscode-remote.remote-ssho
-    golang.go
-    github.copilot
-  ];
-};
+    enable = true;
+    package = pkgs.vscode;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      ms-python.python
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-ssh
+      golang.go
+      github.copilot
+    ];
+  };
 }
