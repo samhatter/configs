eb207ff07d8a0ca0b2a4d10655abeba37651359e
Samantha Vincent <sammyvincent2001@gmail.com>
2026-01-25 12:53:46 -0800

    swapping out k3 for virtualization config

--- nix/hosts/arcanis/configuration.nix
+++ nix/hosts/arcanis/configuration.nix
@@ -8,6 +8,8 @@
     ../../profiles/base.nix
     ../../users/samantha.nix
     # ./modules/caddy.nix
+    ../modules/k3s.nix
+    ../modules/firewall.nix
   ];
 
   networking.hostName = "arcanis";
@@ -24,18 +26,16 @@
     ];
   };
 
-  virtualisation = {
-    docker = {
-      enable = true;
-      liveRestore = true;
-      daemon.settings = {
-        experimental = true;
-        metrics-addr = "0.0.0.0:9323";
-      };
-    };
-  };
-
-  networking.firewall.enable = false;
+  # virtualisation = {
+  #   docker = {
+  #     enable = true;
+  #     liveRestore = true;
+  #     daemon.settings = {
+  #       experimental = true;
+  #       metrics-addr = "0.0.0.0:9323";
+  #     };
+  #   };
+  # };
 
   fileSystems."/storage" = {
     device = "/dev/pool/storage";
--- nix/hosts/arcanis/modules/firewall.nix
+++ nix/hosts/arcanis/modules/firewall.nix
@@ -1,7 +1,7 @@
 {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 200 80 443 32400 5349 7882 ];
+    allowedTCPPorts = [ 200 80 443 32400 5349 7882 6443 ];
     allowedUDPPorts = [ 3487 7882 ];
 
     allowedUDPPortRanges = [ { from = 50100; to = 50200; } ];
