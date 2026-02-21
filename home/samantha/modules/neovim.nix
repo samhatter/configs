0c183a7c7c70277936ca3f9492dd23df165e8a61
samhatter <sammyvincent2001@gmail.com>
2026-02-21 13:15:03 -0800

    refactor LSP keymaps to use autocommand for better context handling

--- home/samantha/modules/neovim.nix
+++ home/samantha/modules/neovim.nix
@@ -107,10 +107,21 @@
         },
       }
 
-      -- Keymaps
-      vim.keymap.set("n", "<leader>f", function()
-          vim.lsp.buf.format({ async = true })
-      end, { desc = "Format file with LSP" })
+      -- LSP keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
     '';
   };
 }
